b4036f8f77bff519317f6eca0a89224c68f0737e
Dewan <shakibrocks2042@gmail.com>
2023-01-29 20:33:36 +0600

    update-2

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,18 +1,18 @@
 import "./Airbnb.css"
 import star from "../assets/star_1.png"
 
-export const Card  = (props) => {
+export const Card  = ({image,stock,rating,title,price}) => {
     return (
         <div className="Card_div">
-            <img src={props.image} alt="img 01" className="c_img_01" />
-                <span className="stock">{props.stock}</span>
+            <img src={image} alt="img 01" className="c_img_01" />
+                <span className="stock">{stock}</span>
                 <div className="star_country">
                     <img src={star} alt="star image" className="star_img"/>
-                    <p className="rating">{props.rating}<span className="change_clr">{props.from}</span></p>
+                    <p className="rating">{rating}<span className="change_clr">{props.from}</span></p>
                 </div>
                 <div className="price_person">
-                    <p className="person_titile">{props.title}</p>
-                    <p className="price_title">{props.price} <span className="change_clr_2">/ person</span></p>
+                    <p className="person_titile">{title}</p>
+                    <p className="price_title">{price} <span className="change_clr_2">/ person</span></p>
                 </div>
         </div>
     )
